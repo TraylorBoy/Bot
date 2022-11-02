9351a212e76174388cc889eba77f2d758b852148
TraylorBoy <traylorboy@protonmail.com>
2022-11-02 14:48:22 -0600

    fixing publish

--- botboy.py
+++ botboy.py
@@ -1,5 +1,5 @@
-"""Multithreading & processing worker that executes functions and prints the
-result"""
+"""Multithreading & processing worker that executes functions and prints the result"""
+__version__ = '1.0.1'
 
 from core.bot import Bot
 
